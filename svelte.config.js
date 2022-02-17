28363563de9fd85d36faefc382fdd4403b4ae646
BuildTools <cephas8080@outlook.com>
2022-02-17 10:29:28 -0100

    firstss

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
