b5b8684f87be2776627f250e5039b089ab29d16a
BuildTools <cephas8080@outlook.com>
2022-02-16 23:31:14 -0100

    first

---
+++ src/routes/index.svelte
@@ -0,0 +1,14 @@
+<script>
+    const SKYKEY = import.meta.env.VITE_KEY;
+    async function getSKY(){
+        let response = await fetch("https://api.hypixel.net/skyblock/profiles?key="+SKYKEY+"&uuid=6b350daa18d946e9a5a08cd6102d4b6e");
+        let output = await response.json();
+        return output;
+    }
+    const out = getSKY();
+</script>
+{#await out}
+<p>waiting</p>
+{:then output}
+{output.success}
+{/await}
