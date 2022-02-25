1b445c932eb265d775af502ba264c8e74bd6a92a
BuildTools <cephas8080@outlook.com>
2022-02-25 18:44:21 -0100

    test

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,6 +9,17 @@
     left: 0;
     z-index: -1;
     }
+    .textspecial {
+        background: linear-gradient(
+            to right,
+            #bf00c1 0%,
+            #FE5F55 30%,
+            #DDD92A 50%
+        );
+        background-clip: text;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+    }
 </style>
-<h1 class="secondarycolor">This is the index page</h1>
+<h1 class="secondarycolor textspecial">The all in one skyblock website for you</h1>
 <div class="back-bg"></div>
